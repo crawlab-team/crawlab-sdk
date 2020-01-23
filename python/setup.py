e2c681c4da765de6a0a221ce5fb9583788a06de3
marvzhang <tikazyq@163.com>
2020-01-23 12:00:02 +0800

    updated setup.py

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 import crawlab.config
 
-with open('README.md', 'r') as f:
+with open('README.rst', 'r') as f:
     long_description = f.read()
 
 setup(
