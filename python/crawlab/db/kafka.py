7101376bf8e680203837f6a9dc70b1968896595f
marvzhang <tikazyq@163.com>
2020-07-04 17:56:06 +0800

    added kafka integration

--- python/crawlab/db/kafka.py
+++ python/crawlab/db/kafka.py
@@ -2,12 +2,12 @@ import json
 
 from kafka import KafkaProducer
 
-from crawlab.utils.config import get_data_source, get_collection
+from crawlab.utils.config import get_data_source
 
 
 def get_producer():
     ds = get_data_source()
-    if ds.get('username') is not None:
+    if ds.get('username') is not None and len(ds.get('username')) > 0:
         return KafkaProducer(
             sasl_mechanism="PLAIN",
             security_protocol='SASL_PLAINTEXT',
@@ -17,10 +17,11 @@ def get_producer():
         )
     else:
         return KafkaProducer(
-            bootstrap_servers=ds.get('host').split(',')
+            bootstrap_servers=f'{ds.get("host")}:{ds.get("port")}'
         )
 
 
 def send_msg(item):
+    ds = get_data_source()
     producer = get_producer()
-    producer.send(get_collection(), json.dumps(item))
+    producer.send(ds.get('database'), json.dumps(item).encode('utf-8'))
