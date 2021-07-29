a6efdb84fc3f799011377a264072a639c29acd5c
marvzhang <tikazyq@163.com>
2021-07-29 16:34:58 +0800

    updated version

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='crawlab-sdk',
-    version='0.6.b20210729_1625',
+    version='0.6.b20210729_1634',
     packages=find_packages(),
     url='https://github.com/crawlab-team/crawlab-sdk',
     license='BSD-3-Clause',
