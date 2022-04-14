95b310c52ee728632c669c57833637144aacec9e
Marvin Zhang <tikazyq@163.com>
2022-04-14 21:33:27 +0800

    fixed es auth issue

--- python/crawlab/db/es.py
+++ python/crawlab/db/es.py
@@ -7,7 +7,13 @@ from crawlab.utils.config import get_data_source
 
 def get_client() -> elasticsearch.Elasticsearch:
     ds = get_data_source()
-    return elasticsearch.Elasticsearch(hosts=[{'host': ds.get('host'), 'port': ds.get('port')}])
+    base_auth = None
+    if ds.get('username') is not None and ds.get('password') is not None:
+        base_auth = f'{ds.get("username")}:{ds.get("password")}'
+    return elasticsearch.Elasticsearch(
+        hosts=[{'host': ds.get('host'), 'port': ds.get('port')}],
+        basic_auth=base_auth,
+    )
 
 
 def index_item(item):
