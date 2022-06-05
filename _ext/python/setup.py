5ff35b5297f6c66bb2b91b13c18de3d63015a352
Marvin Zhang <tikazyq@163.com>
2022-06-05 15:08:05 +0800

    updated version

--- _ext/python/setup.py
+++ _ext/python/setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='crawlab-sdk',
-    version='0.6.0-1',
+    version='0.6.0-2',
     packages=find_packages(),
     url='https://github.com/crawlab-team/crawlab-sdk',
     license='BSD-3-Clause',
