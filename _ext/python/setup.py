9af6a195c04dacfcc1f6878e054749ae6bce2d8f
Marvin Zhang <tikazyq@163.com>
2022-07-13 11:24:34 +0800

    chore: updated package version

--- _ext/python/setup.py
+++ _ext/python/setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as f:
 
 setup(
     name='crawlab-sdk',
-    version='0.6.0-6',
+    version='0.6.0-7',
     packages=find_packages(),
     url='https://github.com/crawlab-team/crawlab-sdk',
     license='BSD-3-Clause',
@@ -30,4 +30,3 @@ setup(
         ]
     }
 )
-
